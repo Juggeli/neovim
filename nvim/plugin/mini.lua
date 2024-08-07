a19cfbe72e34ecae3dfc1d990dd2750971fcc131
Jukka Alavesa <juggeli@gmail.com>
2024-08-07 19:26:29 +0300

    move mini plugins to their own file

---
+++ nvim/plugin/mini.lua
@@ -0,0 +1,22 @@
+if vim.g.did_load_mini_plugin then
+  return
+end
+vim.g.did_load_mini_plugin = true
+
+require('mini.statusline').setup()
+require('mini.git').setup()
+require('mini.diff').setup {
+  view = {
+    style = 'sign',
+    signs = { add = '▎', change = '▎', delete = '' },
+  },
+}
+require('mini.tabline').setup()
+require('mini.comment').setup()
+require('mini.surround').setup()
+require('mini.indentscope').setup {
+  symbol = '│',
+}
+require('mini.icons').setup()
+MiniIcons.mock_nvim_web_devicons()
+require('mini.pick').setup()
--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -6,28 +6,12 @@ vim.g.did_load_plugins_plugin = true
 -- many plugins annoyingly require a call to a 'setup' function to be loaded,
 -- even with default configs
 
+
 require('which-key').setup()
 require('dressing').setup()
-require('mini.statusline').setup()
-require('mini.git').setup()
-require('mini.diff').setup {
-  view = {
-    style = 'sign',
-    signs = { add = '▎', change = '▎', delete = '' },
-  },
-}
-require('mini.tabline').setup()
-require('mini.comment').setup()
-require('mini.surround').setup()
-require('mini.indentscope').setup {
-  symbol = '│',
-}
-require('mini.icons').setup()
-MiniIcons.mock_nvim_web_devicons()
 require('stay-centered').setup {
   skip_filetypes = { 'minifiles' },
 }
-require('mini.pick').setup()
 require('auto-save').setup {
   execution_message = {
     enabled = false,
