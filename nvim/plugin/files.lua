5ed8ac3cfb0a3ba941cd72d2379fff168eac4106
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-12-09 15:53:07 +0200

    close mini.files window after opening file

--- nvim/plugin/files.lua
+++ nvim/plugin/files.lua
@@ -5,7 +5,13 @@ vim.g.did_load_files_plugin = true
 
 local files = require('mini.files')
 
-files.setup()
+files.setup {
+  mappings = {
+    -- Always close window after opening file
+    go_in = '',
+    go_in_plus = 'l',
+  },
+}
 
 local minifiles_toggle = function()
   if not MiniFiles.close() then
