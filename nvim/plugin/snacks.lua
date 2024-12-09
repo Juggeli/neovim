cd327bfea8942797e107859c22a15184593de407
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-12-09 16:54:30 +0200

    add more snacks

--- nvim/plugin/snacks.lua
+++ nvim/plugin/snacks.lua
@@ -11,6 +11,7 @@ local ivy = require('telescope.themes').get_ivy {
 require('snacks').setup {
   notifier = { enabled = true },
   bigfile = { enabled = true },
+  words = { enabled = true },
   dashboard = {
     preset = {
       keys = {
@@ -40,3 +41,57 @@ require('snacks').setup {
     },
   },
 }
+
+local keymap = vim.keymap
+
+keymap.set('n', '<leader>gf', function()
+  Snacks.lazygit.log_file()
+end, { desc = 'Lazygit current file history' })
+
+keymap.set('n', '<leader>gg', function()
+  Snacks.lazygit()
+end, { desc = 'Lazygit' })
+
+keymap.set('n', '<leader>gl', function()
+  Snacks.lazygit.log()
+end, { desc = 'Lazygit log (cwd)' })
+
+keymap.set({ 'n', 't' }, ']]', function()
+  Snacks.words.jump(vim.v.count1)
+end, { desc = 'Next reference' })
+
+keymap.set({ 'n', 't' }, '[[', function()
+  Snacks.words.jump(-vim.v.count1)
+end, { desc = 'Prev reference' })
+
+keymap.set('n', '<leader>n', function()
+  Snacks.notifier.show_history()
+end, { desc = 'Notification history' })
+
+keymap.set('n', '<leader>bd', function()
+  Snacks.bufdelete()
+end, { desc = 'Delete buffer' })
+
+keymap.set('n', '<leader>cR', function()
+  Snacks.rename.rename_file()
+end, { desc = 'Rename file' })
+
+keymap.set('n', '<leader>.', function()
+  Snacks.scratch()
+end, { desc = 'Toggle scratch buffer' })
+
+keymap.set('n', '<leader>S', function()
+  Snacks.scratch.select()
+end, { desc = 'Select scratch buffer' })
+
+keymap.set('n', '<leader>n', function()
+  Snacks.notifier.show_history()
+end, { desc = 'Notification history' })
+
+keymap.set('n', '<leader>gB', function()
+  Snacks.gitbrowse()
+end, { desc = 'Git browse' })
+
+keymap.set('n', '<leader>un', function()
+  Snacks.notifier.hide()
+end, { desc = 'Dismiss all notifications' })
