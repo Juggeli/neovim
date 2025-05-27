28fdb40bfca9d51687f447258e0d85b387488eac
Jukka Alavesa <juggeli@gmail.com>
2025-05-27 23:17:36 +0300

    fix blink config

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -5,52 +5,41 @@ end
 vim.g.did_load_completion_plugin = true
 
 require('blink.cmp').setup {
-  -- 'default' for mappings similar to built-in completion
-  -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-  -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-  -- see the "default configuration" section below for full documentation on how to define
-  -- your own keymap.
+  -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  -- 'super-tab' for mappings similar to vscode (tab to accept)
+  -- 'enter' for enter to accept
+  -- 'none' for no mappings
+  --
+  -- All presets have the following mappings:
+  -- C-space: Open menu or open docs if already open
+  -- C-n/C-p or Up/Down: Select next/previous item
+  -- C-e: Hide menu
+  -- C-k: Toggle signature help (if signature.enabled = true)
+  --
+  -- See :h blink-cmp-config-keymap for defining your own keymap
   keymap = { preset = 'default' },
 
   appearance = {
-    -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-    -- Useful for when your theme doesn't support blink.cmp
-    -- will be removed in a future release
-    use_nvim_cmp_as_default = false,
-    -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
     -- Adjusts spacing to ensure icons are aligned
     nerd_font_variant = 'mono',
   },
 
-  -- default list of enabled providers defined so that you can extend it
-  -- elsewhere in your config, without redefining it, via `opts_extend`
+  -- (Default) Only show the documentation popup when manually triggered
+  completion = { documentation = { auto_show = false } },
+
+  -- Default list of enabled providers defined so that you can extend it
+  -- elsewhere in your config, without redefining it, due to `opts_extend`
   sources = {
-    default = {
-      'lsp',
-      'path',
-      'luasnip',
-      'buffer',
-    },
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
   },
 
-  snippets = {
-    expand = function(snippet)
-      require('luasnip').lsp_expand(snippet)
-    end,
-    active = function(filter)
-      if filter and filter.direction then
-        return require('luasnip').jumpable(filter.direction)
-      end
-      return require('luasnip').in_snippet()
-    end,
-    jump = function(direction)
-      require('luasnip').jump(direction)
-    end,
-  },
+  -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+  -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+  -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+  --
+  -- See the fuzzy documentation for more information
+  fuzzy = { implementation = 'prefer_rust_with_warning' },
 
-  -- experimental auto-brackets support
-  -- completion = { accept = { auto_brackets = { enabled = true } } }
-
-  -- experimental signature help support
   signature = { enabled = true },
 }
